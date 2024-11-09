4c9c5a0583f3b32a187a38ff3fca64b0afb68e18
Alain <alain.devcs@gmail.com>
2024-11-09 05:56:33 +0100

    Add feed fields

--- internal/models/feed.go
+++ internal/models/feed.go
@@ -7,28 +7,75 @@ import (
 
 // RSS represents an RSS feed.
 type RSS struct {
-	XMLName xml.Name `xml:"rss"`
-	Channel Channel  `xml:"channel"`
+	XMLName          xml.Name `xml:"rss"`
+	Version          string   `xml:"version,attr"`
+	ContentNamespace string   `xml:"xmlns:content,attr"`
+	Channel          Channel  `xml:"channel"`
+}
+
+type Content struct {
+	XMLName xml.Name `xml:"content:encoded"`
+	Content string   `xml:",cdata"`
 }
 
 // Channel represents the RSS channel.
 type Channel struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	Language    string `xml:"language"`
-	PubDate     string `xml:"pubDate"`
-	LastBuild   string `xml:"lastBuildDate"`
-	Items       []Item `xml:"item"`
+	TextInput      *TextInput
+	Image          *Image
+	XMLName        xml.Name `xml:"channel"`
+	Category       string   `xml:"category,omitempty"`
+	Docs           string   `xml:"docs,omitempty"`
+	Copyright      string   `xml:"copyright,omitempty"`
+	ManagingEditor string   `xml:"managingEditor,omitempty"`
+	WebMaster      string   `xml:"webMaster,omitempty"`
+	PubDate        string   `xml:"pubDate,omitempty"`
+	LastBuildDate  string   `xml:"lastBuildDate,omitempty"`
+	Description    string   `xml:"description"`
+	Generator      string   `xml:"generator,omitempty"`
+	Language       string   `xml:"language,omitempty"`
+	Cloud          string   `xml:"cloud,omitempty"`
+	Title          string   `xml:"title"`
+	Rating         string   `xml:"rating,omitempty"`
+	SkipHours      string   `xml:"skipHours,omitempty"`
+	SkipDays       string   `xml:"skipDays,omitempty"`
+	Link           string   `xml:"link"`
+	Items          []Item   `xml:"item"`
+	TTL            int      `xml:"ttl,omitempty"`
+}
+
+// Image represents the RSS image.
+type Image struct {
+	XMLName xml.Name `xml:"image"`
+	URL     string   `xml:"url"`
+	Title   string   `xml:"title"`
+	Link    string   `xml:"link"`
+	Width   int      `xml:"width,omitempty"`
+	Height  int      `xml:"height,omitempty"`
+}
+
+// TextInput represents the RSS text input.
+type TextInput struct {
+	XMLName     xml.Name `xml:"textInput"`
+	Title       string   `xml:"title"`
+	Description string   `xml:"description"`
+	Name        string   `xml:"name"`
+	Link        string   `xml:"link"`
 }
 
 // Item represents an RSS item.
 type Item struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
-	GUID        string `xml:"guid"`
+	XMLName     xml.Name `xml:"item"`
+	Title       string   `xml:"title"`       // required
+	Link        string   `xml:"link"`        // required
+	Description string   `xml:"description"` // required
+	Content     *Content
+	Author      string `xml:"author,omitempty"`
+	Category    string `xml:"category,omitempty"`
+	Comments    string `xml:"comments,omitempty"`
+	Enclosure   *RssEnclosure
+	GUID        *RssGUID // Id used
+	PubDate     string   `xml:"pubDate,omitempty"` // created or updated
+	Source      string   `xml:"source,omitempty"`
 }
 
 // Feed represents a feed in our system.
@@ -39,4 +86,21 @@ type Feed struct {
 	Description string    `json:"description"`
 	LastFetch   time.Time `json:"last_fetch"`
 	Items       []Item    `json:"items"`
+}
+
+// RssEnclosure represents the RSS enclosure.
+type RssEnclosure struct {
+	// RSS 2.0 <enclosure url="http://example.com/file.mp3" length="123456789" type="audio/mpeg" />
+	XMLName xml.Name `xml:"enclosure"`
+	URL     string   `xml:"url,attr"`
+	Length  string   `xml:"length,attr"`
+	Type    string   `xml:"type,attr"`
+}
+
+// RssGUID represents the RSS guid.
+type RssGUID struct {
+	// RSS 2.0 <guid isPermaLink="true">http://inessential.com/2002/09/01.php#a2</guid>
+	XMLName     xml.Name `xml:"guid"`
+	ID          string   `xml:",chardata"`
+	IsPermaLink string   `xml:"isPermaLink,attr,omitempty"` // "true", "false", or an empty string
 }
